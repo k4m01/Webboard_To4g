b8934444f41764544b730d9b5c1a91cf2318d523
k4m01 <152370438+k4m01@users.noreply.github.com>
2024-01-03 15:44:53 +0700

    งานครั้งที่ 4  POST & GET

---
+++ post.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+        <h1 style="text-align: center;">Webboard KakKak</h1>
+        <hr>
+
+        <p style="text-align: center;">ต้องการดูกระทู้หมายเลข <?php echo $_GET['id']?></p><br>
+        <table style="border: 2px solid black; width: 40%;" align="center">
+            <tr><td colspan="2" style="background-color: #6CD2FE;">แสดงความคิดเห็น</td></tr>
+            <tr><td align="center"><textarea name ="text" rows="10" cols=" 60"></textarea></td></tr>
+            <tr><td align="center" ><input type="submit" value="ส่งข้อความ" ></td></tr>
+        </table>
+        <p style="text-align: center;"><a href="index.html"> กลับไปหน้าหลัก </a></p>
+</body>
+</html>
